112ae99c6aecfd4f3ed7cf4d42ee2016403c9871
mawkeye <mawkeye@gmail.com>
2025-10-31 13:21:31 +0100

    CH2L5: Refactor to stats.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
-
-def get_num_words(text):
-    return len(text.split())
 
 def main():
     path_to_file = "books/frankenstein.txt"
