11c90272a2b94ab7d50dc84e00674f19f9b9ff02
mawkeye <mawkeye@gmail.com>
2025-10-31 13:20:10 +0100

    CH2L4: add count words

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
-        
+
+def get_num_words(text):
+    return len(text.split())
+
 def main():
     path_to_file = "books/frankenstein.txt"
     content = get_book_text(path_to_file)
-    print(content)
+    num_words = get_num_words(content)
+    print(f"Found {num_words} total words")
 
 main()
